c4e7d97d680405add6da09f2a5cc751f87dd1b5d
HenrySTGiron <164544178+HenrySTGiron@users.noreply.github.com>
2024-04-03 15:50:40 -0700

    Add files via upload

---
+++ weather-dag-load.py
@@ -0,0 +1,128 @@
+from airflow import DAG
+from datetime import timedelta, datetime
+from airflow.operators.python import PythonOperator
+import snowflake.connector
+import json
+import os
+import logging
+
+# Setup logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+os.environ["no_proxy"]="*"
+
+conn = snowflake.connector.connect(
+    user='HENRYG',
+    password='Darksouls123!',
+    account='FALHXOU-RAB73489',
+    warehouse='CLIMATE_FASHION_WAREHOUSE',
+    database='Climate_Fashion_DB',
+    schema='Climate_Schema'
+)
+cursor = conn.cursor()
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': datetime(2024, 3, 4),
+    'email':'henryhungryhen@gmail.com',
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 2,
+    'retry_delay': timedelta(minutes=2)
+}
+
+def final_cleanup():
+    cwd = os.getcwd()
+    logger.info("Current working directory: %s", cwd)
+    file_path = os.path.join(os.getcwd(), '02128__GHCND USW00014739_2023-03-01_2024-03-01.json')
+
+    with open('2023-03-01_2024-03-10_date_dimension.json', 'r') as date_file:
+        date_dimension_data = json.load(date_file)
+        logger.info('Opened date dimension file!')
+    # Create a dictionary mapping date_value to date_id
+    date_id_map = {entry['date_value']: entry['date_id'] for entry in date_dimension_data}
+
+    # Original JSON data
+    with open(file_path, 'r') as climate_fact_file:
+        climate_fact_data = json.load(climate_fact_file)
+        logger.info('Opened climate fact file!')
+
+    # Replace "date" field with "date_id" based on the mapping and remove the "date" key
+    for entry in climate_fact_data:
+        date_value = entry.pop('date').split('T')[0]
+        entry['date_id'] = date_id_map.get(date_value, None)
+
+    # Ensure 'is_training_data' remains boolean
+    for entry in climate_fact_data:
+        entry['is_training_data'] = bool(entry['is_training_data'])
+        
+    arranged_data = []
+    for entry in climate_fact_data:
+        arranged_entry = {
+        'date_id': entry['date_id'],
+        'station_id': entry['station'],
+        "min_temp": entry["TMIN"],
+        "max_temp": entry["TMAX"],
+        "snowfall": entry["SNOW"],
+        "snow_depth": entry.get("SNWD", 0),  # Handle missing SNWD with default 0
+        "rain": entry.get("PRCP", 0),  # Handle missing PRCP with default 0
+        "is_training_data": entry["is_training_data"] 
+        }
+        arranged_data.append(arranged_entry)
+
+    # Write the modified JSON data to a new file
+    with open('modified_json_data.json', 'w') as output_file:
+        json.dump(arranged_data, output_file, indent=4)
+
+    logger.info('Modified JSON data file created successfully.')
+
+def weather_load():
+    try:
+        create_json_file_format = '''
+        CREATE OR REPLACE FILE FORMAT my_json_format
+        TYPE = 'JSON'
+        COMPRESSION = 'AUTO'
+        STRIP_OUTER_ARRAY = TRUE;
+        '''
+        cursor.execute(create_json_file_format)
+
+        check_file_existence = 'LIST @PUBLIC.JSON_STAGE'
+        cursor.execute(check_file_existence)
+
+        results = cursor.fetchall()
+        for row in results:
+            logger.info(row[0])
+
+        copy_into_sql = '''
+        COPY INTO CLIMATE_FACT_TABLE
+        FROM @PUBLIC.JSON_STAGE
+        FILES = ('modified_json_data.json')
+        FILE_FORMAT = my_json_format
+        MATCH_BY_COLUMN_NAME=CASE_INSENSITIVE
+        ON_ERROR=ABORT_STATEMENT
+        '''
+
+        cursor.execute(copy_into_sql)
+    except snowflake.connector.errors.ProgrammingError as e:
+        logger.error("Error loading data: %s", e)
+    finally:
+        cursor.close()
+        conn.close()
+
+with DAG('weather_load_dag', default_args=default_args, schedule_interval=None) as dag:
+    
+    final_climate_data_cleanup =PythonOperator(
+        task_id='final_climate_data_cleanup_task',
+        python_callable=final_cleanup,
+        dag=dag
+    )
+    
+    transform_data = PythonOperator(
+        task_id='weather-load-task',
+        python_callable=weather_load,
+        dag=dag
+    )
+
+final_climate_data_cleanup >> transform_data
---
+++ weather-dag-snowflake-create.py
@@ -0,0 +1,214 @@
+import logging
+from airflow import DAG
+from datetime import timedelta, datetime
+from airflow.operators.dagrun_operator import TriggerDagRunOperator
+from airflow.operators.python import PythonOperator
+import snowflake.connector
+import os
+
+os.environ['no_proxy']='*'
+
+# Set up logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': datetime(2024, 3, 4),
+    'email':'henryhungryhen@gmail.com',
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 2,
+    'retry_delay': timedelta(minutes=2)
+}
+
+def create_climate_schema_and_table():
+    try:
+        conn = snowflake.connector.connect(
+            user='HENRYG',
+            password='Darksouls123!',
+            account='FALHXOU-RAB73489'
+            )
+        logger.info('Connection successful')
+    except snowflake.connector.errors.DatabaseError as e:
+        logger.error('Error connecting to Snowflake: %s', e)
+
+    cursor = conn.cursor()
+    create_warehouse_sql = """
+    CREATE WAREHOUSE IF NOT EXISTS Climate_Fashion_warehouse
+    WAREHOUSE_SIZE = 'XSMALL'
+    AUTO_SUSPEND = 1 
+    AUTO_RESUME = TRUE 
+    """
+    cursor.execute(create_warehouse_sql)
+    # Create Database
+    create_database_sql = 'CREATE DATABASE IF NOT EXISTS Climate_Fashion_DB'
+    cursor.execute(create_database_sql)
+
+    # Use Database
+    use_database_sql = 'USE DATABASE Climate_Fashion_DB'
+    cursor.execute(use_database_sql)
+
+    # Create Schema
+    create_schema_sql = 'CREATE SCHEMA IF NOT EXISTS Climate_Schema'
+    cursor.execute(create_schema_sql)
+
+    logger.info('Tables have begun to be created!')
+
+    create_date_table_sql = '''
+    CREATE OR REPLACE TABLE Climate_Schema.Date_Table (
+    date_id INT PRIMARY KEY,
+    date_value DATE,
+    season VARCHAR(20)
+    );
+    '''
+    cursor.execute(create_date_table_sql)
+    logger.info('Date table created!')
+
+    create_station_table_sql = '''
+    CREATE OR REPLACE TABLE Climate_Schema.Station_Table (
+    station_id VARCHAR(100) PRIMARY KEY,
+    zipcode VARCHAR(10),
+    city VARCHAR(50),
+    state VARCHAR(2)
+    );
+    '''
+    cursor.execute(create_station_table_sql)
+    logger.info('Station table created!')
+
+    create_fact_table_sql = '''
+    CREATE OR REPLACE TABLE Climate_Schema.Climate_Fact_Table (
+    date_id INT ,
+    station_id VARCHAR(100),
+    min_temp INT,
+    max_temp INT,
+    snowfall INT,
+    snow_depth INT,
+    rain INT,
+    is_training_data BOOLEAN,
+    FOREIGN KEY (date_id) REFERENCES Climate_Schema.Date_Table(date_id),
+    FOREIGN KEY (station_id) REFERENCES Climate_Schema.Station_Table(station_id)
+    );
+    '''
+    cursor.execute(create_fact_table_sql)
+    logger.info('Fact table created!')
+
+    sql_stage = '''
+    CREATE OR REPLACE STAGE json_stage
+        DIRECTORY = (ENABLE = TRUE);'''
+
+    cursor.execute(sql_stage)
+    logger.info('Stage created!')
+    conn.close()
+    logger.info('Connection closed!')
+
+def create_fashion_schema_and_table():
+    try:
+        conn = snowflake.connector.connect(
+            user='HENRYG',
+            password='Darksouls123!',
+            account='FALHXOU-RAB73489',
+            warehouse='Climate_Fashion_warehouse',
+            database='Climate_Fashion_DB'
+            )
+        logger.info('Connection successful')
+    except snowflake.connector.errors.DatabaseError as e:
+        logger.error('Error connecting to Snowflake: %s', e)
+
+    cursor = conn.cursor()
+
+    use_database_sql = 'USE DATABASE Climate_Fashion_DB'
+    cursor.execute(use_database_sql)
+
+    create_schema_sql = 'CREATE OR REPLACE SCHEMA Fashion_Schema'
+    cursor.execute(create_schema_sql)
+
+    logger.info('Tables are being created!')
+
+    create_customer_stats_table_sql = '''
+    CREATE OR REPLACE TABLE Fashion_Schema.Customer_Stats_Table (
+    customer_id INT PRIMARY KEY,
+    statistic_id INT,
+    statistic_value VARCHAR(255)
+    );
+    '''
+    cursor.execute(create_customer_stats_table_sql)
+    logger.info('Customer stats table created!')
+
+    create_customer_address_table_sql = """
+    CREATE OR REPLACE TABLE Fashion_Schema.Customer_Address_Table (
+    customer_id INT,
+    address_id INT PRIMARY KEY,
+    primary_address BOOLEAN,
+    address_1 VARCHAR(255),
+    address_2 VARCHAR(255),
+    city VARCHAR(100),
+    state VARCHAR(100),
+    zipcode VARCHAR(20),
+    country VARCHAR(100)
+    );
+    """
+    cursor.execute(create_customer_address_table_sql)
+    logger.info('Customer address table created!')
+
+    create_customer_table_sql = """
+    CREATE OR REPLACE TABLE Fashion_Schema.Customer_Table (
+    customer_id INT PRIMARY KEY,
+    address_id INT,
+    FOREIGN KEY (customer_id) REFERENCES Fashion_Schema.Customer_Stats_Table(customer_id),
+    FOREIGN KEY (address_id) REFERENCES Fashion_Schema.Customer_Address_Table(address_id)
+    );
+    """
+    cursor.execute(create_customer_table_sql)
+    logger.info('Customer table created!')
+
+    create_order_line_table_sql = """
+    CREATE OR REPLACE TABLE Fashion_Schema.Order_Line_Table (
+    order_id INT PRIMARY KEY,
+    order_line_id INT,
+    item_id INT,
+    order_date TIMESTAMP_NTZ
+    );
+    """
+    cursor.execute(create_order_line_table_sql)
+    logger.info('Order line table created!')
+
+    create_order_table_sql = """
+    CREATE OR REPLACE TABLE Fashion_Schema.Order_Table (
+    order_date TIMESTAMP_NTZ PRIMARY KEY,
+    order_id INT,
+    customer_id INT,
+    FOREIGN KEY (customer_id) REFERENCES Fashion_Schema.Customer_Table(customer_id),
+    FOREIGN KEY (order_id) REFERENCES Fashion_Schema.Order_Line_Table(order_id)
+    );
+    """
+    cursor.execute(create_order_table_sql)
+    logger.info('Order table created!')
+
+   
+
+with DAG('snowflake-stage-create-dag', 
+    default_args=default_args,
+    schedule_interval = '@daily',
+    catchup = False) as dag:
+
+
+    create_climate_infra = PythonOperator(
+        task_id = 'snowflake_climate_infra_task',
+        python_callable = create_climate_schema_and_table,
+        dag = dag
+    ) 
+    create_fashion_infra = PythonOperator(
+        task_id = 'snowflake_fashion_infra_task',
+        python_callable = create_fashion_schema_and_table,
+        dag = dag
+    )
+    trigger_dag = TriggerDagRunOperator(
+        task_id='trigger_extract_dag',
+        trigger_dag_id='weather_api_extract_and_transform_dag',
+        dag=dag,
+        trigger_rule='all_success'
+    )
+
+    [create_climate_infra, create_fashion_infra] >> trigger_dag
